233a3613490928b12bb687f281f9b536cf3504a2
unknown <Administrator@MEDICITY.CO.IN>
2024-09-19 17:29:12 +0530

    static changed

--- src/Constant/Static.jsx
+++ src/Constant/Static.jsx
@@ -1,7 +1,7 @@
 // export const API_URL = "https://localhost:5000/api";
 
-export const API_URL = "http://travancoremedicity.in:50031";
+export const API_URL = "https://travancoremedicity.in:50031";
 
 export const PUBLIC_NAS_FOLDER = "http://192.168.22.5";
 
-export const Url = "http://travancoremedicity.in:50031";
+export const Url = "https://travancoremedicity.in:50031";
